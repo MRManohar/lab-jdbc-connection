e7d9373d2c54b04606032e0d393e3fb6b590ea47
MRManohar <lmmrider99@gmail.com>
2020-07-27 15:52:00 +0530

    FSJ0420A109

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,9 +5,17 @@ import java.sql.Connection;
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
-		
-	// Fill your code
-		
+	public static void main(String args[]) throws Exception {
+		ConnectionManager cm = new ConnectionManager();
+		Connection con = null;
+		con = cm.getConnectionManager();
+		try {
+			if(con != null) {
+				System.out.println("Connection Established.");
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Check your Connection.");
+		}
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,31 @@
 package utility;
 
-
-
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionManager {
-	
-	
+	public Connection getConnectionManager() throws Exception {
+		
+		Properties pro = loadPropertiesFile();
+		
+		String driver = pro.getProperty("driver");
+		String url = pro.getProperty("url");
+		String username = pro.getProperty("username");
+		String password = pro.getProperty("password");
+		
+		Class.forName(driver);
+		
+		Connection con = DriverManager.getConnection(url,username,password);
+		
+		return con; 
+	}
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
 }
